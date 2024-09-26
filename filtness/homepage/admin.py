8772145a0e1a6e74354b53830fee0ce3c7dc6988
Evgenyi Filatov <bleidin12345@yandex.ru>
2024-09-26 12:47:59 +0700

    fixed referral

--- filtness/homepage/admin.py
+++ filtness/homepage/admin.py
@@ -1,28 +1,19 @@
 from django.contrib import admin
-
-
-from .models import Clients, Referral
+from .models import Clients
 
 
 @admin.register(Clients)
 class ClientsAdmin(admin.ModelAdmin):
     list_display = (
+        'id',
         'first_name',
         'last_name',
         'birthday',
         'created_at',
+        'referral',
         'is_visiting'
         )
     list_editable = ('is_visiting',)
-    list_filter = ('is_visiting',)  # Фильтрация по полям
-    ordering = ('-created_at',)  # Сортировка по дате создания
-    date_hierarchy = 'created_at'  # Поддержка иерархии даты
-
-
-@admin.register(Referral)
-class ReferralAdmin(admin.ModelAdmin):
-    list_display = (
-        'client',
-        'referred_client'
-        )
-    list_editable = ('referred_client',)
+    list_filter = ('is_visiting',)
+    ordering = ('-created_at',)
+    date_hierarchy = 'created_at'
--- filtness/homepage/models.py
+++ filtness/homepage/models.py
@@ -6,12 +6,20 @@ class Clients(models.Model):
     last_name = models.CharField('Фамилия', max_length=50)
     birthday = models.DateField(
         'День рождения клиента',
-        )
+    )
     created_at = models.DateTimeField(
         'Добавлено', auto_now_add=True)
     is_visiting = models.BooleanField(
         'Ходит или нет', default=True,
         help_text='Снимите галочку, если клиент не ходит.')
+    referral = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='referred_clients',
+        verbose_name='Реферал'
+    )
 
     class Meta:
         verbose_name = 'клиент'
@@ -19,25 +27,3 @@ class Clients(models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
-
-
-class Referral(models.Model):
-    client = models.ForeignKey(
-        Clients,
-        on_delete=models.CASCADE,
-        related_name='referrals',
-        verbose_name='Клиент-реферал'
-        )
-    referred_client = models.ForeignKey(
-        Clients,
-        on_delete=models.CASCADE,
-        related_name='referral_of',
-        verbose_name='Реферал')
-
-    class Meta:
-        verbose_name = 'реферал'
-        verbose_name_plural = 'Рефералы'
-
-    def __str__(self):
-        return f'{self.client} - {self.referred_client}'
-
