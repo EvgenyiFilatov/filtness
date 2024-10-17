efac99f1f33cb24a844badcf032374e1ff1f26ad
Evgenyi Filatov <bleidin12345@yandex.ru>
2024-10-17 22:14:50 +0700

    add edit and change password in profile

--- filtness/filtness/urls.py
+++ filtness/filtness/urls.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homepage.urls')),
     path('about/', include('about.urls')),
     path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('blog:index'),
+        ),
+        name='registration',
+    ),
     path('user/', include('user.urls')),
     path('sign_for_training/', include('sign_for_training.urls')),
 ]
